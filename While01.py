4c769a329b2d770f6fa888debd486483c767f400
Qilichevv <behruzqilichevv23@gmail.com>
2022-06-29 08:56:47 -0400

    did

--- While01.py
+++ While01.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    
+    idx=0
+    ans=0
+    while idx <= len(s):
+        ans+= s[idx].isdigit()
+        idx+=1
+        
+    return ans
+print(main(cd342f))
